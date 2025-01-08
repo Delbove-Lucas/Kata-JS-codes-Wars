1a200c4c992d6c13dc3e6c417ebbc7f3e1cc3990
Delbove-Lucas <lucasdelbove1@gmail.com>
2025-01-08 14:43:00 +0100

    Feat : ajout kata ky8

---
+++ Grasshopper_Create_the_rooms.js
@@ -0,0 +1,17 @@
+let rooms = {
+    room1: {
+        name: "Salon",
+        description: "Un salon avec un canapé et une télévision.",
+        completed: false
+    },
+    room2: {
+        name: "Cuisine",
+        description: "Une cuisine avec un réfrigérateur et un four.",
+        completed: false
+    },
+    room3: {
+        name: "Bibliothèque",
+        description: "Une bibliothèque avec des livres et un fauteuil.",
+        completed: false
+    }
+};
